a69eff906cdb0febf816f203eeaf57e4178d3e35
DyllanRuth <dyllanruth@gmail.com>
2021-03-04 09:29:54 +0000

    servlet test one hello world /hello

--- portfolio/src/main/java/com/google/sps/servlets/HelloWorldServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/HelloWorldServlet.java
@@ -10,7 +10,12 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/hello")
 public class HelloWorldServlet extends HttpServlet {
 
-  @Override
+    /**
+     *Added because with code throws an error
+     */
+    private static final long serialVersionUID = 1L;
+
+    @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
     response.setContentType("text/html;");
     response.getWriter().println("<h1>Hello world!</h1>");
